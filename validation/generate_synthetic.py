64477d3a6a75885d3255e6f0a40d1d41af3264b4
olivares-j <avijer@gmail.com>
2026-02-11 12:00:39 +0100

    Adds dynamic mass ranges

--- validation/generate_synthetic.py
+++ validation/generate_synthetic.py
@@ -11,11 +11,21 @@ file_mlp_logg = dir_mlps+"Logg_l13_s256/mlp.pkl"
 base_name = "a{0:d}_d{1:d}_n{2:d}_s{3:d}"
 
 list_of_ages = list(range(20,420,20))
-list_of_distances  = [50,100,150,200]
+list_of_distances  = [50]#,100,150,200]
 n_stars   = 10
 seed      = 0
 
-mass_limits = [0.1,4.5]
+def mass_limits(age):
+	if age < 160:
+		return [0.1,4.5]
+	elif age < 200:
+		return [0.1,4.0]
+	elif age < 300:
+		return [0.1,3.3]
+	elif age <= 400:
+		return [0.1,3.0]
+	else:
+		sys.exit("No available age!")
 
 def phasespace_args(distance):
 	args = {
@@ -30,12 +40,12 @@ def phasespace_args(distance):
 				}}
 	return args
 
-def isochrones_args(model,age,mass_limits):
+def isochrones_args(model,age):
 	args = {
 		"model":model,
 		"age": float(age),
 		"MIST_args":{
-			"mass_limits":mass_limits,
+			"mass_limits":mass_limits(age),
 			"metallicity":0.012,
 			"Av": 0.0
 			},
@@ -43,7 +53,7 @@ def isochrones_args(model,age,mass_limits):
 				"file_mlp_phot":file_mlp_phot,
 				"file_mlp_teff":file_mlp_teff,
 				"file_mlp_logg":file_mlp_logg,
-				"mass_limits":mass_limits,
+				"mass_limits":mass_limits(age),
 				},          
 		"bands":["G","BP","RP"]#,"gP1", "rP1", "iP1", "zP1", "yP1","J", "H", "Ks"],
 		}
@@ -59,7 +69,7 @@ for age in list_of_ages:
 
 		ama = Amasijo(
 					phasespace_args=phasespace_args(distance),
-					isochrones_args=isochrones_args(model,age,mass_limits),
+					isochrones_args=isochrones_args(model,age),
 					seed=seed)
 		ama.generate_cluster(file_data,n_stars=n_stars,angular_correlations=None)
 		ama.plot_cluster(file_plot=file_plot)
